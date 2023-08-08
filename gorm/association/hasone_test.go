2674e385264454a1f6ef61d7bc55864bdb7c18c8
ahuigo <1781999+ahuigo@users.noreply.github.com>
2023-08-09 00:05:08 +0800

    gorm:hasone

--- gorm/association/hasone_test.go
+++ gorm/association/hasone_test.go
@@ -8,28 +8,27 @@ import (
 	"gorm.io/gorm"
 )
 
-// `BelongParent` belongs to `BelongChild`, `ChildID` is the foreign key(依赖child)
-type BelongParent struct {
-	gorm.Model
-	Name         string
-	Age          uint8
-	MemberNumber sql.NullString
-	ActivatedAt  sql.NullTime
-	ChildID      uint
-	BelongChild  BelongChild `gorm:"foreignKey:child_id;references:pid;"` // BelongParent(child_id) has one child
-	// BelongChild   BelongChild `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
-}
-
-type BelongChild struct {
-	Pid   uint `gorm:"primarykey"`
-	Cname string
-}
-
 /*
 *
 常用于group: belong to company
 */
 func TestHasOne(t *testing.T) {
+	// `BelongParent` belongs to `BelongChild`, `ChildID` is the foreign key(依赖child)
+	type BelongChild struct {
+		Pid   uint `gorm:"primarykey"`
+		Cname string
+	}
+	type BelongParent struct {
+		gorm.Model
+		Name         string
+		Age          uint8
+		MemberNumber sql.NullString
+		ActivatedAt  sql.NullTime
+		ChildID      uint
+		BelongChild  BelongChild `gorm:"foreignKey:child_id;references:pid;"` // BelongParent(child_id) has one child
+		// BelongChild   BelongChild `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	}
+
 	db := tt.Db
 	child := BelongChild{
 		Pid:   1,
